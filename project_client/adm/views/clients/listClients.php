e9a79fe04b570fb2c7123922abc23762c34db677
Lucas Lange <58046434+lucasmullerlange@users.noreply.github.com>
2020-09-25 08:40:14 -0300

    Add files via upload

---
+++ project_client/adm/views/clients/listClients.php
@@ -0,0 +1,36 @@
+<h1>Lista de Clientes</h1>
+<table class="table">
+    <tr>
+        <th> Código</th>
+        <th> Nome</th>
+        <th> Email</th>
+        <th> Telefone</th>
+        <th> Endereço</th>
+        <th> Editar </th>
+        <th> delete </th>
+
+    </tr>
+    <?php
+    foreach ($arrayClients as $client) {
+    ?>
+    <tr>
+
+        <td><?= $client["idClient"] ?></td>
+        <td><?= $client["name"] ?></td>
+        <td><?= $client["email"] ?></td>
+        <td><?= $client["phone"] ?></td>
+        <td><?= $client["address"] ?></td>
+        <td>
+            <a href="?controller=clients&action=updateClient&id=<?=$client
+                 ['idClient']?>" class="btn btn-warning"> Editar </a>
+
+        </td>
+        <td>
+            <a href="?controller=client&action=deleteClient&id" <?=$client
+                ['idClient']?> class="btn btn-danger">Deletar</a>
+        </td>
+    </tr>
+<?php
+  }
+?>
+</table>
